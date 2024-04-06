a8299d7bc27209e17b6b71b0e372bf7c72a1aa09
biruk <birukendalew090@gmail.com>
2024-04-06 14:32:28 +0300

    add user model

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ const errorHandler = require('./handlers/errorHandler');
 const app=express();
 app.use(express.json());
 
-
+//model
+require('./models/user.model.js')
 
 app.use(errorHandler);
-mongoose.connect(process.env.mongoDB).then(()=>{
+mongoose.connect(process.env.mongoDB,{}).then(()=>{
     app.listen(4000,()=>{
         console.log("RUNNING");
     })
---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose')
+
+const userSchema=new mongoose.Schema({
+        name:{
+            type:String,
+            required:[true,'Please enter your name']
+        },
+        email:{
+            type:String,
+            required:[true,'Please enter your email'],
+            unique:true
+        },
+        password:{
+            type:String,
+            required:[true,'Please enter your password']
+        },
+        balance:{
+            type:Number,
+            required:[true,'Please enter your balance'],
+            default:0
+        },
+
+})
+
+const userModel=mongoose.model('user',userSchema)
+module.exports=userModel
